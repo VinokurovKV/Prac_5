7c44870552fa4eeacdff39298d6886a2525fece9
VinokurovKV <kirikvin8490@gmail.com>
2025-09-16 10:58:16 +0300

    Commit at 10:58

---
+++ 20250916/0/empty.py
@@ -0,0 +1,5 @@
+l = eval(input())
+if not l:
+    print("EMPTY")
+else:
+    print("NOT EMPTY")
---
+++ 20250916/0/triangle.py
@@ -0,0 +1,5 @@
+a, b, c = eval(input())
+if a and b and c and a + b > c and a + c > b and b + c > a:
+    print("Triangle")
+else: 
+    print("Not triangle")
